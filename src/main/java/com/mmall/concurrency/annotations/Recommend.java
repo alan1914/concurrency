b2102eae378f09634b7d65c768f85f8d2309b32d
db0n15 <db0n15@163.com>
2019-02-17 09:23:24 +0800

    注解

---
+++ src/main/java/com/mmall/concurrency/annotations/Recommend.java
@@ -0,0 +1,18 @@
+package com.mmall.concurrency.annoations;
+
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 课程里用来标识【线程不安全】的类或者写法
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface Recommend {
+
+    String value() default "";
+
+}
